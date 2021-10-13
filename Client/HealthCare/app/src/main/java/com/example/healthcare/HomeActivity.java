5f8d85e942b387d91b26884fa22e69027de34e1a
semin <semin015123@gmail.com>
2021-10-14 02:12:43 +0900

    홈 화면, 세팅 화면 끝, 게시판 화면, 검색화면(네이버지도) 설정 해야함

--- Client/HealthCare/app/src/main/java/com/example/healthcare/HomeActivity.java
+++ Client/HealthCare/app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -2,18 +2,30 @@ package com.example.healthcare;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class HomeActivity extends AppCompatActivity {
-    private String nickname, name, birthday;
+    private Button btn_drawer;
+    private Button btn_settings;
+    private Button btn_community;
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
 
     private TextView tv_home_nickname;
     private TextView tv_home_name;
     private TextView tv_home_birthday;
 
     private Intent intent;
+
+
+    private String nickname, name, birthday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +34,14 @@ public class HomeActivity extends AppCompatActivity {
 
         init_view();
         init_intent();
-
+        btn_clicked();
 
         tv_home_nickname.setText(nickname);
         tv_home_name.setText(name);
         tv_home_birthday.setText(birthday);
     }
 
-    private void init_intent(){
+    private void init_intent() {
         intent = getIntent();
         nickname = intent.getStringExtra("nickname");
         name = intent.getStringExtra("name");
@@ -37,9 +49,43 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    private void init_view(){
-        tv_home_nickname = (TextView)findViewById(R.id.tv_home_nickname);
-        tv_home_name = (TextView)findViewById(R.id.tv_home_name);
-        tv_home_birthday = (TextView)findViewById(R.id.tv_home_birthday);
+    private void init_view() {
+        btn_drawer = (Button) findViewById(R.id.btn_drawer);
+        btn_community = (Button) findViewById(R.id.btn_community);
+        btn_settings = (Button) findViewById(R.id.btn_settings);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_home);
+        drawerView = (View) findViewById(R.id.drawer);
+
+        tv_home_nickname = (TextView) findViewById(R.id.tv_home_nickname);
+        tv_home_name = (TextView) findViewById(R.id.tv_home_name);
+        tv_home_birthday = (TextView) findViewById(R.id.tv_home_birthday);
+
+    }
+
+    private void btn_clicked() {
+        btn_drawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+        btn_community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+                Intent intent = new Intent(HomeActivity.this, CommunityActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+                Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- Client/HealthCare/app/src/main/java/com/example/healthcare/SettingsActivity.java
+++ Client/HealthCare/app/src/main/java/com/example/healthcare/SettingsActivity.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InfoActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_settings);
     }
 }
